d8d44529ef9da1230ce5629de7a961af171ea698
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2022-11-17 22:36:16 -0300

    primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import NotificationButton from "./components/NotificationButton" 
+/*
+  App principal para rodar os componentes
+*/
 
 function App() {
-  return(
+  return (
+    <>
     <h1>Hello World</h1>
-  )
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
+    )
 }
 
 export default App
